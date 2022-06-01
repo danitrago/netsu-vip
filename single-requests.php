b2409e15870590ce50c89e20334d62ef46007df7
danitrago <danitrago.7@gmail.com>
2022-06-01 13:50:36 -0500

    using composition to print form

--- single-requests.php
+++ single-requests.php
@@ -44,44 +44,7 @@ $current_user = wp_get_current_user();
 				</div>
 
 
-				<!-- New Post Form -->
-				<div id="postbox">
-					<form id="new_post" name="new_post" method="post" action="" enctype="multipart/form-data">
-
-						<!-- post name -->
-						<p><label for="title">Título</label><br />
-							<input type="text" id="title" value="<?php echo get_the_ID() ?> - OFERTA" readonly tabindex="1" size="20" name="title" />
-						</p>
-
-						<!-- post name -->
-						<p><label for="integrador">Integrador</label><br />
-							<input type="text" id="integrador" value="<?php echo $current_user->user_login; ?>" readonly tabindex="1" size="20" name="integrador" />
-						</p>
-
-						<!-- post Valor -->
-						<p><label for="valor">Valor</label><br />
-							<input type="number" id="valor" value="" tabindex="1" size="20" name="valor" required />
-						</p>
-
-						<!-- post name -->
-						<p><label for="asesor_comercial">Asesor comercial</label><br />
-							<input type="text" id="asesor_comercial" value="" tabindex="1" size="20" name="asesor_comercial" required />
-						</p>
-
-						<!-- post name -->
-						<p><label for="adjunto">Adjunto</label><br />
-							<input type="file" id="adjunto" value="" tabindex="1" size="20" name="adjunto" required accept="application/pdf" />
-						</p>
-						
-						<p align="right"><input type="submit" value="Publish" tabindex="6" id="submit" name="submit" /></p>
-
-						<input type="hidden" name="action" value="new_post" />
-						<?php wp_nonce_field('new-post'); ?>
-					</form>
-
-					<?php postingOffer(); ?>
-
-				</div>
+				<?php formPostOffer() ?>
 
 			</div>
 	<?php
